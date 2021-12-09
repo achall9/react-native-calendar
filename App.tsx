1293cbc8c92cbdef141f5f960b9325c0ff8c5c3f
achall9 <alexandros.hall9@gmail.com>
2021-12-09 08:25:05 -0500

    init

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+import SpaceTop from './src/components/SpaceTop';
+import HomeScreen from './src/screens/Home';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <SpaceTop top={40} />
+      <HomeScreen />
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
